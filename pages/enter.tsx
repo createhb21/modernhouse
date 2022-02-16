d79a51d651437830b96228190079d178a49c4f56
createhb21 <alsghk9701@gmail.com>
2022-02-16 10:22:44 +0900

    refactor paths

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,10 +1,10 @@
 import type { NextPage } from 'next';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import Button from '../components/Button';
-import Input from '../components/Input';
-import useMutation from '../libs/client/useMutation';
-import { cls } from '../libs/client/utils';
+import Button from '@components/Button';
+import Input from '@components/Input';
+import useMutation from '@libs/client/useMutation';
+import { cls } from '@libs/client/utils';
 
 interface EnterForm {
   email?: string;
