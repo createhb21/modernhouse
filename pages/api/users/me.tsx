beb871a1a772fe7093ec4f03a6d0baa99e80a2da
createhb21 <alsghk9701@gmail.com>
2022-02-17 22:35:47 +0900

    Profile handle

--- pages/api/users/me.tsx
+++ pages/api/users/me.tsx
@@ -15,20 +15,17 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>,
 ) {
-  const { token } = req.body;
-  const exists = await client.token.findUnique({
-    where: { payload: token },
-    // include: { user: true },
+  console.log(req.session.user);
+  const profile = await client.user.findUnique({
+    where: { id: req.session.user?.id },
   });
-  if (!exists) return res.status(404).end();
-  req.session.user = {
-    id: exists.userId,
-  };
-  await req.session.save();
-  res.status(200).end();
+  res.json({
+    ok: true,
+    profile,
+  });
 }
 
-export default withIronSessionApiRoute(withHandler('POST', handler), {
+export default withIronSessionApiRoute(withHandler('GET', handler), {
   cookieName: 'livecommerce',
   password:
     '1238121231231238012fadsasd1234qasdasfaeqweqweasd134123aasdqwe1rfeafgewr983901454821',
