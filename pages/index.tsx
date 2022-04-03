79f84c07f70c772f02d943dcc2167d30b1575cab
createhb21 <alsghk9701@gmail.com>
2022-04-04 03:00:27 +0900

    Counting Relationships

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,9 +7,14 @@ import Head from 'next/head';
 import useSWR from 'swr';
 import { Product } from '@prisma/client';
 
+interface ProductWithCount extends Product {
+  _count: {
+    favs: number;
+  };
+}
 interface ProductResponse {
   ok: boolean;
-  products: Product[];
+  products: ProductWithCount[];
 }
 
 const Home: NextPage = () => {
@@ -29,7 +34,7 @@ const Home: NextPage = () => {
             title={product.name}
             price={product.price}
             comments={1}
-            hearts={1}
+            hearts={product._count.favs}
           />
         ))}
         <FloatingButton href="/products/upload">
